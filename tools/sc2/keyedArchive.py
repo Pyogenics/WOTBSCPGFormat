83751fe6f93bed1ff99c266afda61f407ee60d82
Pyogenics <py0g3n1cs@gmail.com>
2023-08-19 14:08:41 +0100

    Moved components into individual files

--- tools/sc2/keyedArchive.py
+++ tools/sc2/keyedArchive.py
@@ -7,15 +7,12 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
-from sys import argv
+
 from enum import Enum
 
-class DescriptorFileTypes(Enum):
-    NONE = -1
-    SceneFile = 0
-    ModelFile = 1
+from common import ImportError
 
-class KeyedArchiveTypes(Enum):
+class Types(Enum):
     #TODO: Get actual values for these, using placeholders for now
     NONE = 0
     BOOLEAN = 1
@@ -53,10 +50,10 @@ class KeyedArchive:
 
         # read value
         match valueType:
-            case KeyedArchiveTypes.NONE.value:
+            case Types.NONE.value:
                 value = stream.read(4) #XXX: Take a wild guess
                 return value
-            case KeyedArchiveTypes.STRING.value:
+            case Types.STRING.value:
                 length = int.from_bytes(stream.read(4), "little")
                 raise ImportError(f"l{length}")
                 string = stream.read(length)
@@ -90,45 +87,3 @@ class KeyedArchive:
             self.items[key] = value
 
         raise ImportError(self.items)
-
-class SC2Importer:
-    def readDataNodes(self, stream, count):
-        for _ in range(count):
-            node = KeyedArchive()
-            node.loadFromFileStream(stream)
-
-    def importFromFileStream(self, stream):
-        # Check magic
-        if stream.read(4) != b"SFV2":
-            raise ImportError("Invalid magic! Are you sure this is a Scene File v2?")
-
-        # Read header
-        version = int.from_bytes(stream.read(4), "little")
-        nodeCount = int.from_bytes(stream.read(4), "little")
-
-        self.version = version
-        self.nodeCount = nodeCount
-
-        # Read version tags
-        versionTags = {}
-        if version >= 14:
-            versionTagsArchive = KeyedArchive()
-            versionTagsArchive.loadFromFileStream(stream)
-            versionTags = versionTagsArchive.items
-
-        # Read descriptor
-        descriptorSize = 0
-        descriptorFileType = DescriptorFileTypes.NONE
-        if version >= 10:
-            descriptorSize = int.from_bytes(stream.read(4), "little")
-            descriptorFileType = int.from_bytes(stream.read(4), "little")
-            unknownField = int.from_bytes(stream.read(4), "little") #TODO: Investigate this
-
-        # Read data nodes
-        if version >= 2:
-            dataNodeCount = int.from_bytes(stream.read(4), "little")
-            self.readDataNodes(stream, dataNodeCount)
-
-importer = SC2Importer()
-with open(argv[1], "rb") as f:
-    importer.importFromFileStream(f)
